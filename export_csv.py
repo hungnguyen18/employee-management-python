1896242ce40680cc7b6ec199bf2d8f9757eab6ad
hungnguyen18 <hungtrewq3214@gmail.com>
2023-11-03 20:16:01 +0700

    Fix utf-8

--- export_csv.py
+++ export_csv.py
@@ -6,6 +6,8 @@ ctk = customtkinter
 
 def export_to_csv(data):
     input_dialog = ctk.CTkInputDialog(text="File csv name", title="Export")
-    with open(f"{input_dialog.get_input()}.csv", "w", newline="") as file:
+    with open(
+        f"{input_dialog.get_input()}.csv", "w", newline="", encoding="utf-8"
+    ) as file:
         writer = csv.writer(file)
         writer.writerows(data)
